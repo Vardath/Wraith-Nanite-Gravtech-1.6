2f404eb9523de5171cb83170dc52a8b6d94ab975
Vardath <59270526+Vardath@users.noreply.github.com>
2026-09-19 23:12:05 +1000

    D108: fix RimWorld 1.6 managed API mismatch

--- Source/WraithNaniteGravtech/Wraith/WraithHybridization.cs
+++ Source/WraithNaniteGravtech/Wraith/WraithHybridization.cs
@@ -144,7 +144,7 @@ namespace WraithNaniteGravtech
         public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
         {
             base.Apply(target, dest);
-            Pawn victim = target.Pawn;
+            Pawn victim = target.Thing as Pawn;
             if (!ValidBiologicalTarget(victim))
                 return;
 
